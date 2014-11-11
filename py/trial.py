baf733be62356cb473c9b7b57ee89833c246355e
Hari Krishna Vetharenian <nus.harikv@gmail.com>
2014-11-11 22:36:04 +0800

    Started writing new file from scratch

--- py/trial.py
+++ py/trial.py
@@ -1,29 +1,3 @@
-# __author__ = 'Akaash'
-#
-# import cv2
-# import numpy as np
-#
-# # mouse callback function
-# def draw_circle(event,x,y,flags,param):
-#     print flags, param
-#     if event == cv2.EVENT_LBUTTONDOWN:
-#         print x,y
-#         cv2.circle(img,(x,y),2,(255,0,0), 1, lineType=8, shift = 0)
-#     elif event == cv2.EVENT_MOUSEMOVE:
-#         print x,y
-#
-# # Create a black image, a window and bind the function to window
-# img = cv2.imread('../project.jpeg', cv2.CV_LOAD_IMAGE_COLOR)
-# cv2.namedWindow('image')
-# cv2.setMouseCallback('image',draw_circle)
-#
-#
-# while(1):
-#     cv2.imshow('image',img)
-#     if cv2.waitKey(20) & 0xFF == 27:
-#         break
-# cv2.destroyAllWindows()
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,11 +6,10 @@ import matplotlib.patches as patches
 from projection import return_projected_point, degtorad
 import math
 
-model = [((0,0), np.array([-10, -27, 40]), (149,165,199)), ((0,0), np.array([15, -27, 40]), (65,105,164)), \
-         ((0,0), np.array([-10, 30, -3]), (0,0,0)), ((0,0), np.array([15, 30, -3]), (0,0,0)), \
-         ((0,0), np.array([-10, 30, 40]), (0,0,0)), ((0,0), np.array([15, 30, 40]), (0,0,0))]
+model = []
 
-polygon = [(320.0, 502.0), (657.0, 502.0), (931.0, 528.0), (0.0, 528.0)]
+# polygon = [(320.0, 502.0), (657.0, 502.0), (931.0, 528.0), (0.0, 528.0)]
+polygon = [(55, 70)]
 drawing = False # true if mouse is pressed
 mode = 'V' # if True, draw rectangle. Press 'm' to toggle to curve
 parallelX = False
@@ -59,6 +32,7 @@ def draw_circle(event,x,y,flags,param):
                 iy = int(polygon[-1][1])
                 parallelY = False
             cv2.circle(img,(ix,iy),1,(0,0,255),-1)
+            print (ix, iy)
             polygon.append((ix*1.0,iy*1.0))
 
 def getDivisions(pt1, pt2, granularity):
@@ -77,7 +51,7 @@ def startScanLine(points):
     if numberDivision1 != numberDivision2:
         print "error"
         return
-    in_polygon = [(-10, 0, -3), (10, 0, -3), (10, -27, -3), (-10, -27, -3)]
+    in_polygon = [(-10, 0, -3), (10, 0, -3), (10, -10, -3), (-10, -10, -3)]
     # x_coords = []
     # y_coords = []
     # z_coords = []
@@ -95,39 +69,7 @@ def startScanLine(points):
         numberHorizontalDivisions = len(points[index])
         array3DHorizontal = getDivisions(array3D1[index], array3D2[index], numberHorizontalDivisions)
         model.extend(zip(points[index], array3DHorizontal, zip([img[x[1]][x[0]][0] for x in points[index]], [img[x[1]][x[0]][1] for x in points[index]], [img[x[1]][x[0]][2] for x in points[index]])))
-    # print model
-    # print [m[1] for m in model]
-    # perpendicularDirection = int(raw_input('Enter the perpendicular direction\n'))
-    # diff = 1
-    # start = 0
-    # end = 0
-    # perpendicularMax = 0
-    # perpendicularMin = 0
-    # if scanDirection == 0 :
-    #     start = min(x_coords)
-    #     end = max(x_coords)
-    #     diff = max(x_coords) - min(x_coords)
-    # elif scanDirection == 1:
-    #     start = min(y_coords)
-    #     end = max(y_coords)
-    #     diff = max(y_coords) - min(y_coords)
-    # elif scanDirection == 2:
-    #     start = min(z_coords)
-    #     end = max(z_coords)
-    #     diff = max(z_coords) - min(z_coords)
-    # if perpendicularDirection == 0 :
-    #     perpendicularMax = max(x_coords)
-    #     perpendicularMin =  min(x_coords)
-    # elif perpendicularDirection == 1:
-    #     perpendicularMax = max(y_coords)
-    #     perpendicularMin =  min(y_coords)
-    # elif perpendicularDirection == 2:
-    #     perpendicularMax = max(z_coords)
-    #     perpendicularMin =  min(z_coords)
-    # pixelDifference = len(points)
-    # Increase_factor = diff*1.0/pixelDifference
-    # for i in np.arange(start, end, Increase_factor):
-    #     print i
+
 
 def processPolygon():
     length = len(polygon)
@@ -158,34 +100,33 @@ def projectModel():
     projected_points = []
     for pt in [m[1] for m in model]:
         projected_point = return_projected_point(pt, [0, -27.1, 0], viewing_angle_in_radians)
-        projected_points.append(projected_point)
+        if projected_point is not None:
+            projected_points.append(projected_point)
     pixel_coords = quantize(projected_points)
     print len(model)
     for (pt, color) in zip(pixel_coords, [m[2] for m in model]):
         # print pt, color
-        out_img[pt[0]][pt[1]] = np.array(list(color))
+        out_img[pt[1]][pt[0]] = np.array(list(color))
     cv2.imwrite("../test.jpg", out_img)    
 
 def quantize(input_list):
-    max_x = max(input_list[0])
-    max_y = max(input_list[1])
-    min_x = min(input_list[0])
-    min_y = min(input_list[1])
-    x_ratio = (max_x - min_x)
-    y_ratio = (max_y - min_y)
+    max_x = max([x[0] for x in input_list])
+    max_y = max([x[1] for x in input_list])
+    min_x = min([x[0] for x in input_list])
+    min_y = min([x[1] for x in input_list])
+    x_ratio = (min_x - max_x)
+    y_ratio = (min_y - max_y)
     print x_ratio
     print y_ratio
-    global out_img 
-    out_img = np.zeros(shape=(x_ratio+1, y_ratio+1, 3))
     output_list = []
     for (x,y) in input_list:
-        print x, y
-        output_list.append([int(((x - min_x)*x_ratio*10000)/(max_x - min_x))/10000, int(((y - min_y)*y_ratio*10000)/(max_y - min_y))/10000])
+        # print x, y
+        output_list.append([int(((x - max_x)*10000*932)/x_ratio)/10000, int(((y - max_y)*10000*699)/y_ratio)/10000])
     return output_list
 
-im = cv2.imread("../project_rs.jpg",cv2.CV_LOAD_IMAGE_COLOR)
-img = cv2.imread("../project_rs.jpg",cv2.CV_LOAD_IMAGE_COLOR)
-img_out = cv2.imread("../white.jpg",cv2.CV_LOAD_IMAGE_COLOR)
+im = cv2.imread("../cube.jpg",cv2.CV_LOAD_IMAGE_COLOR)
+img = cv2.imread("../cube.jpg",cv2.CV_LOAD_IMAGE_COLOR)
+out_img = cv2.imread("../white.jpg",cv2.CV_LOAD_IMAGE_COLOR)
 print img.shape
 print img[0].shape
 print img[1].shape
